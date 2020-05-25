ded9af7d17263df0557dab9d07d62d41edbf23b0
giwi97 <giwanthawijekoon@gmail.com>
2020-05-26 04:17:41 +0530

    Search bar created

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ const weatherapi = {
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search...."/>
 
+        </div>
+      </main>
     </div>
   );
 }
