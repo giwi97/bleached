8b6d5671e070d5b6c26799f77dcad1d01575ac69
giwi97 <giwanthawijekoon@gmail.com>
2020-05-26 02:56:43 +0530

    Addded api key and base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const weatherapi = {
 
+  key: "4fc99fa56dae5040518049ca5d5b2d7d",
+  base: "https://api.openweathermap.org/data/2.5"
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
